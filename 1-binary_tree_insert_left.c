4e0c0bba6de1129bafd4aa72be0f08095f0e0547
cvalverde96 <cvalverde96@gmail.com>
2024-07-29 18:14:23 -0400

    subiendo 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function para insertar nodo en
+ * child izquierdo
+ * @parent: poitner del parent
+ * @value: valor en nodo
+ * Return: pointer
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		if (parent == NULL)
+		{
+			return (NULL);
+		}
+
+		new->parent = parent;
+		new->n = value;
+		new->left = NULL;
+		new->right = NULL;
+		new->parent->left = new;
+
+	}
+	return (new);
+}
