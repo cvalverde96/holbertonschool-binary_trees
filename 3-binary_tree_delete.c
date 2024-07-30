180109042442fe39248165814c6d81fc1049ee7f
cvalverde96 <cvalverde96@gmail.com>
2024-07-30 13:02:57 -0400

    subiendo 3 con recursion

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -14,8 +14,8 @@ void binary_tree_delete(binary_tree_t *tree)
 
 	else
 	{
-		free(tree->right);
-		free(tree->left);
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
 		free(tree);
 	}
 }
