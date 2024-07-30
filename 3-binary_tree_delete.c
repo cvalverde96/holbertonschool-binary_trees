82bb4797bcf8c6eede25a5ccc05ab39a3f613b26
cvalverde96 <cvalverde96@gmail.com>
2024-07-30 07:43:06 -0400

    subiendo la 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - funcion que libera free
+ * @tree: pointer to root
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		exit(1);
+	}
+
+	else
+	{
+		free(tree);
+	}
+}
