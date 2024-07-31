5ee971c0b9add86da5eb5af97d5a178da208ff51
cvalverde96 <cvalverde96@gmail.com>
2024-07-31 15:43:20 -0400

    subiendo 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,13 +8,14 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	int n;
+
 
 	if (tree == NULL || *func == NULL)
 	{
 		return;
 	}
 
-	n = tree->n;
-	(*func)(n);
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
