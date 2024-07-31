7911cbf8c3a35e678d638ce4a8fe9c1661dff88d
cvalverde96 <cvalverde96@gmail.com>
2024-07-31 12:27:53 -0400

    subiendo 6 sin recursion

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function para hacer preorder traversal
+ * @tree: pointer a root
+ * @func: function pointer
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	int n;
+
+	if (tree == NULL || *func == NULL)
+	{
+		return;
+	}
+
+
+	/**
+	 * if (*func == NULL)
+	 * {
+	 * return;
+	 * }
+	 */
+
+	n = tree->n;
+	(*func)(n);
+
+	n = tree->left->n;
+	(*func)(n);
+
+	n = tree->right->n;
+	(*func)(n);
+}
